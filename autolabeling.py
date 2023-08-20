7f6c3e36e0658dbb69799cc469144fdebc7e10ee
amulet1989 <muletdelosreyes89@gmail.com>
2023-08-20 01:26:23 -0300

    first commit

--- autolabeling.py
+++ autolabeling.py
@@ -51,30 +51,37 @@ def main():
     args = parser.parse_args()
 
     # Run pipeline
-
     # 1- Convert videos to images
-    # get all folders into video_dir
+    # 2- Run autolabel for each image
 
+    # get all folders into video_dir
     video_paths = get_video_folder_paths(args.video)
 
     for video_path in tqdm(video_paths):
-        # create image folder for each video
+        # create image folder for each video folder
         image_dir_path = os.path.join(args.image_dir, os.path.basename(video_path))
         if not os.path.exists(image_dir_path):
             os.mkdir(image_dir_path)
 
+        # create dataset folder for each video folder
+        dataset_dir_path = os.path.join(
+            args.output_images, os.path.basename(video_path)
+        )
+        if not os.path.exists(dataset_dir_path):
+            os.mkdir(dataset_dir_path)
+
         # convert video to images
         video2images(video_path, image_dir_path, args.frame_rate)
 
-    # 2- Run autolabel model
-    autolabel_images(
-        input_folder=args.image_dir,
-        ontology=json.load(open(args.ontology)),
-        box_threshold=args.box_threshold,
-        text_threshold=args.text_threshold,
-        output_folder=args.output_images,
-        extension=args.extension,
-    )
+        # 2- Run autolabel for each image in image_dir_path
+        autolabel_images(
+            input_folder=image_dir_path,
+            ontology=json.load(open(os.path.join(video_path, "ontology.json"))),
+            box_threshold=args.box_threshold,
+            text_threshold=args.text_threshold,
+            output_folder=dataset_dir_path,
+            extension=args.extension,
+        )
 
 
 if __name__ == "__main__":
