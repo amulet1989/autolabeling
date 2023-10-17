c177074a5e10a2b13b72aa0d12ec22da67214a9d
amulet1989 <muletdelosreyes89@gmail.com>
2023-10-17 00:33:08 -0300

    added convertion to yolov1 format

--- src/convert2yolov1.py
+++ src/convert2yolov1.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import yaml
+import zipfile
 
 
 def convert_to_yolov1_format(dataset_path):
@@ -71,6 +72,92 @@ def convert_to_yolov1_format(dataset_path):
     # Comprimir la carpeta "_YOLOV1"
     shutil.make_archive(yolov1_path, "zip", yolov1_path)
     shutil.rmtree(yolov1_path)
+
+
+def convert_to_yolov8_format(yolov1_zip_path, output_dir):
+    # Obtener el nombre del archivo ZIP sin extensión
+    yolov1_dir_name = os.path.splitext(os.path.basename(yolov1_zip_path))[0]
+
+    # Crear una carpeta con el mismo nombre más un sufijo en la carpeta de salida
+    yolov8_dir = os.path.join(output_dir, yolov1_dir_name + "_YOLOV8")
+    os.makedirs(yolov8_dir, exist_ok=True)
+
+    # Extraer el archivo ZIP en la carpeta recién creada
+    with zipfile.ZipFile(yolov1_zip_path, "r") as zip_ref:
+        zip_ref.extractall(yolov8_dir)
+
+    # Rutas de directorios YOLOv8
+    data_dir = os.path.join(yolov8_dir, "data")
+    train_images_dir = os.path.join(yolov8_dir, "train", "images")
+    train_labels_dir = os.path.join(yolov8_dir, "train", "labels")
+    valid_images_dir = os.path.join(yolov8_dir, "valid", "images")
+    valid_labels_dir = os.path.join(yolov8_dir, "valid", "labels")
+
+    # Crear directorios necesarios
+    os.makedirs(data_dir, exist_ok=True)
+    os.makedirs(train_images_dir, exist_ok=True)
+    os.makedirs(train_labels_dir, exist_ok=True)
+    os.makedirs(valid_images_dir, exist_ok=True)
+    os.makedirs(valid_labels_dir, exist_ok=True)
+
+    # Mover imágenes y etiquetas desde obj_Train_data a train/images y train/labels
+    for filename in os.listdir(os.path.join(yolov8_dir, "obj_Train_data")):
+        if filename.endswith(".jpg"):
+            image_path = os.path.join(yolov8_dir, "obj_Train_data", filename)
+            label_filename = filename.replace(".jpg", ".txt")
+            label_path = os.path.join(yolov8_dir, "obj_Train_data", label_filename)
+
+            # Mover imágenes
+            shutil.move(image_path, os.path.join(train_images_dir, filename))
+
+            # Mover etiquetas
+            shutil.move(label_path, os.path.join(train_labels_dir, label_filename))
+
+    # Mover imágenes y etiquetas desde obj_Validation_data a valid/images y valid/labels
+    for filename in os.listdir(os.path.join(yolov8_dir, "obj_Validation_data")):
+        if filename.endswith(".jpg"):
+            image_path = os.path.join(yolov8_dir, "obj_Validation_data", filename)
+            label_filename = filename.replace(".jpg", ".txt")
+            label_path = os.path.join(yolov8_dir, "obj_Validation_data", label_filename)
+
+            # Mover imágenes
+            shutil.move(image_path, os.path.join(valid_images_dir, filename))
+
+            # Mover etiquetas
+            shutil.move(label_path, os.path.join(valid_labels_dir, label_filename))
+
+    # Copiar data.names a obj.names
+    data_names_path = os.path.join(yolov8_dir, "obj.names")
+    obj_names_path = os.path.join(data_dir, "obj.names")
+    shutil.copy(data_names_path, obj_names_path)
+
+    # Crear data.yaml
+    # Obtener nombres de clases
+    obj_names_path = os.path.join(yolov8_dir, "data", "obj.names")
+    with open(obj_names_path, "r") as obj_names_file:
+        class_names = [line.strip() for line in obj_names_file]
+
+    # Crear data.yaml con nc y paths actualizados
+    data_yaml_path = os.path.join(yolov8_dir, "data.yaml")
+    with open(data_yaml_path, "w") as data_yaml_file:
+        data_yaml_file.write("names:\n")
+        for class_name in class_names:
+            data_yaml_file.write(f"  - {class_name}\n")
+        data_yaml_file.write(f"nc: {len(class_names)}\n")
+        data_yaml_file.write(f"train: {os.path.join(yolov8_dir, 'train', 'images')}\n")
+        data_yaml_file.write(f"val: {os.path.join(yolov8_dir, 'valid', 'images')}\n")
+
+    # Mover data.yaml a output_dir
+    # data_yaml_path = os.path.join(yolov8_dir, "data", "data.yaml")
+    # shutil.move(data_yaml_path, os.path.join(yolov8_dir, "data.yaml"))
+
+    # Eliminar archivos y directorios innecesarios
+    for item in os.listdir(yolov8_dir):
+        item_path = os.path.join(yolov8_dir, item)
+        if os.path.isfile(item_path) and item != "data.yaml":
+            os.remove(item_path)
+        elif os.path.isdir(item_path) and item not in ["train", "valid"]:
+            shutil.rmtree(item_path)
 
 
 if __name__ == "__main__":
