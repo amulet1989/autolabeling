799f4572574a55f7218f56064d1978f044b5e83a
amulet1989 <muletdelosreyes89@gmail.com>
2024-07-03 19:09:59 -0300

    last

--- yolo_predict.py
+++ yolo_predict.py
@@ -4,7 +4,7 @@ from src.util import seleccionar_video, seleccionar_imagen
 
 
 model = YOLO(
-    "trained_models/yolov8m_tracking_pilar_15cam_v5.pt"
+    "trained_models/yolov8m_cf_caja_640x480_v9.pt"
 )  # yolov8m_640x480_cf_9cam_v44
 # model = RTDETR("rtdetr-l.pt")  # rtdetr-l.pt
 
@@ -12,7 +12,7 @@ model = YOLO(
 # Create VideoCapture object
 # INPUT_VIDEO = seleccionar_video()
 # INPUT_IMAGE = seleccionar_imagen()
-INPUT_VIDEO = "rtsp://user:pass.@192.168.88.8/live1"
+INPUT_VIDEO = "rtsp://admin:2Mini001.@181.164.198.186:9563//h264/ch1/sub/av_stream"
 
 
 # Read video
@@ -26,10 +26,10 @@ results = model.track(
     stream=True,
     save=False,
     conf=0.4,
-    imgsz=704,
+    imgsz=640,  # 704
     iou=0.7,
     # verbose=False,
-    classes=classes,
+    # classes=classes,
     show=True,
 )  # generator of Results objects
 
@@ -42,40 +42,40 @@ for r in results:
         classe = boxes.cls.tolist()
         label = r.names
         scores = boxes.conf.tolist()  # Confidence scores
-    #     # print("clases:", classe)
-    #     # print("scores:", scores)
-    #     # print("labels:", label)
+#         # print("clases:", classe)
+#         # print("scores:", scores)
+#         # print("labels:", label)
 
-    #     # Draw BBoxes on the image
-    #     # for box, label, score in zip(boxes, labels, scores):
-    #     for i, box in enumerate(boxes.xyxy):
-    #         x1, y1, x2, y2 = map(int, box)  # box
-    #         color = (0, 255, 0)  # Green color
-    #         thickness = 2
+#         # Draw BBoxes on the image
+#         # for box, label, score in zip(boxes, labels, scores):
+#         for i, box in enumerate(boxes.xyxy):
+#             x1, y1, x2, y2 = map(int, box)  # box
+#             color = (0, 255, 0)  # Green color
+#             thickness = 2
 
-    #         cv2.rectangle(image, (x1, y1), (x2, y2), color, thickness)
+#             cv2.rectangle(image, (x1, y1), (x2, y2), color, thickness)
 
-    #         text = f"{label[int(classe[i])]} ({scores[i]:.2f})"
-    #         # print(text)
+#             text = f"{label[int(classe[i])]} ({scores[i]:.2f})"
+#             # print(text)
 
-    #         cv2.putText(
-    #             image,
-    #             text,
-    #             (x1, y1 - 10),
-    #             cv2.FONT_HERSHEY_SIMPLEX,
-    #             1,
-    #             color,
-    #             thickness,
-    #         )
+#             cv2.putText(
+#                 image,
+#                 text,
+#                 (x1, y1 - 10),
+#                 cv2.FONT_HERSHEY_SIMPLEX,
+#                 1,
+#                 color,
+#                 thickness,
+#             )
 
-    # cv2.imshow(win_name, image)
+#     cv2.imshow(win_name, image)
 
-    # # Wait for a key press and check the pressed key
-    # key = cv2.waitKey(1)  # & 0xFF
-    # if key == ord("q"):  # Press 'q' to exit
-    #     break
-    # elif key == ord("n"):  # Press 'n' to show the next image
-    #     continue
+#     # Wait for a key press and check the pressed key
+#     key = cv2.waitKey(1)  # & 0xFF
+#     if key == ord("q"):  # Press 'q' to exit
+#         break
+#     elif key == ord("n"):  # Press 'n' to show the next image
+#         continue
 
 # # Release VideoCapture and destroy windows
 # cap.release()
